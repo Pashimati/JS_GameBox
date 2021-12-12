d0ed1ea1590568586ae1963ca50333430d455132
Pashimati <pashimatii@gmail.com>
2021-12-12 13:58:09 +0300

    getRandomColor

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 let $start = document.querySelector('#start')
 let $game = document.querySelector('#game')
+let $time = document.querySelector('#time')
 
 let score = 0
+let isGameStarted = false
 
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handleBoxClick)
 
 function startGame() {
+    isGameStarted = true
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
+
+    let interval = setInterval(function () {
+        let time = parseFloat($time.textContent)
+
+        if (time <= 0) {
+            clearInterval(interval)
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox()
 }
 
+function endGame() {
+    isGameStarted = false
+}
+
 function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return
+    }
+
     if (event.target.dataset.box) {
         score++
         renderBox()
     }
 }
+
+function getRandomColor() {
+    let color = Math.floor(Math.random() * 16777216).toString(16);
+    return '#000000'.slice(0, -color.length) + color;
+}
+
 
 function renderBox() {
     $game.innerHTML = ''
@@ -30,7 +58,7 @@ function renderBox() {
 
     box.style.height = box.style.width = boxSize + 'px'
     box.style.position = 'absolute'
-    box.style.backgroundColor = '#000'
+    box.style.backgroundColor = getRandomColor()
     box.style.top = getRandom(0, maxTop) + 'px'
     box.style.left = getRandom(0, maxLeft) + 'px'
     box.style.cursor = 'pointer'
